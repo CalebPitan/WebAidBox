c012248081c2d3a1200b34c9a4a920b6e0b6275b
Adepitan Caleb <36812893+CalebPitan@users.noreply.github.com>
2018-12-21 10:30:06 +0100

    Update utils-clickables.js

--- docs/assets/js/utils-clickables.js
+++ docs/assets/js/utils-clickables.js
@@ -1,23 +1,9 @@
 /* !
- * Utiliser (Modern Utils) 
- * utils-clickables.js v 1.0
  * @Author Adepitan Caleb
  * Copyright 2018 RealLongman
  *
- * Dependencies : jQuery;
- * Uses : Facebook pop sound as (utils-pop.m4a|mp3);
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Uses :jQuery Facebook pop sound as (utils-pop.m4a|mp3);
+ * Do as you like no terms no conditions
  * 
  */
 
@@ -27,7 +13,7 @@
   var clickables = [],
       clickable,
       // edit audio path according to file system
-       popSound = new Audio('./assets/sound/utils-pop.mp3');
+       popSound = new Audio('./assets/sound/utils-pop.m4a');
       clickable = document.getElementsByTagName('a');
   clickables.push(clickable);
   clickable = document.getElementsByTagName('button');
